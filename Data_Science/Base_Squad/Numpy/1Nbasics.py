e361d50a2cd0b69d2d06622cbe497834553e24fb
Kevboyz <kevboyz@pm.me>
2025-12-14 16:25:47 -0300

    some ups

--- Data_Science/Base_Squad/Numpy/1Nbasics.py
+++ Data_Science/Base_Squad/Numpy/1Nbasics.py
@@ -6,7 +6,7 @@ arr = np.arange(0, 100, 10).reshape(2, 5)
 rg = np.random.default_rng()
 # print(rg.random((4, 3)))
 
-lin = np.linspace(-10, 10, 6, rstep=True)
+lin = np.linspace(-10, 10, 6)
 # print(lin)
 
 zeros = np.zeros(shape=(3, 3, 3, 3))  # 4 dims  -- ones 0 -> 1
@@ -15,3 +15,5 @@ zeros = np.zeros(shape=(3, 3, 3, 3))  # 4 dims  -- ones 0 -> 1
 empty = np.empty((2, 5))
 # print(empty)  # None empty values
 
+print(arr[0,:])
+print(arr * np.array(2))
